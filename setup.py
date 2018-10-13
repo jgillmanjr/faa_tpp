953f9e0e4e47181044e7759565ffc257880a725c
Jason Gillman Jr <jgillman@liquidweb.com>
2018-10-13 03:08:10 -0400

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='faa_tpp',
+    version='0.1',
+    packages=['faa_tpp'],
+    url='https://github.com/jgillmanjr/faa_tpp',
+    license='',
+    author='Jason Gillman Jr.',
+    author_email='jason@rrfaae.com',
+    description='Get FAA dTPP data'
+)
